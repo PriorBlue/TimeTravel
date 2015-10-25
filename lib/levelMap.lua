9bfb388bcab1c108b1a28b81b6a1952557e9f991
Supersonic112 <supersonic112@live.de>
2015-10-25 13:27:40 +0100

    added editor mode, finished ingame floor tile editor

--- lib/levelMap.lua
+++ lib/levelMap.lua
@@ -1,47 +1,115 @@
 timetravel.levelMap = {}
-timetravel.levelMap.textureSizeX, timetravel.levelMap.textureSizeY = 16, 16
+timetravel.levelMap.textureSizeX, timetravel.levelMap.textureSizeY = 32, 32
+timetravel.levelMap.editor = {}
+timetravel.levelMap.editor.currentTile = 0
+timetravel.levelMap.editor.maxTileNumber = 25
+timetravel.levelMap.editor.lastKeyPress = 0
+timetravel.levelMap.editor.mousepressed = ""
+
+class "LevelMap" {
+	currentMapString = "";
+	defaultMapString = "";
+	mapSizeX, mapSizeY = 0, 0;
+	mapImage = nil;
+	mapBatch = nil;
+	quad = nil;
+}
+
+local function getCharFromNumber(number)
+	return string.char(number+97)
+end
 
 local function getTileFromChar(char)
 	--print("got string"..char)
-	if char == "a" then
-		return 0,0
-	elseif char == "b" then
-		return 0,1
-	elseif char == "c" then
-		return 0,2
+	if type(char) == "number" then--char:len() > 1 then -- we have a number
+		return char%16,(char-(char%16))/16
 	else
-		return 1,0
-	end
-end
-
-function timetravel.levelMap:__init(sizeX, sizeY, mapString) --LevelMap:__init
-	mapImage = love.graphics.newImage("gfx/tileset.png")
-	mapBatch = love.graphics.newSpriteBatch(mapImage,sizeX*sizeY)
-	quad = love.graphics.newQuad(0,0,16,16,mapImage:getWidth(), mapImage:getHeight())
-	m = {}
-	if not(mapString == nil) and (string.len(mapString)==sizeX*sizeY) then
-		for i=0,sizeX-1 do
-			for j=0,sizeY-1 do
-				--print("drawing map from string")
-				local textureX, textureY = getTileFromChar(mapString:sub(1+j+i*sizeX, 1+j+i*sizeX))
-				--print(textureX..", "..textureY)
-				quad:setViewport(textureX,textureY,timetravel.levelMap.textureSizeX,timetravel.levelMap.textureSizeY)
-				mapBatch:add(quad,i*timetravel.levelMap.textureSizeX,j*timetravel.levelMap.textureSizeY)
-			end
-		end
-	else -- map string does not exist or has the wrong length, generate random map
-		if not(string.len(mapString)==sizeX*sizeY) then
-			print("WARNING: map string has the wrong length ("..string.len(mapString).."), should be "..sizeX*sizeY)
-		end
-		for i=0,sizeX-1 do
-			for j=0,sizeY-1 do
-				quad:setViewport(love.math.random(0,4)*16,0,timetravel.levelMap.textureSizeX,timetravel.levelMap.textureSizeY)
-				mapBatch:add(quad,i*timetravel.levelMap.textureSizeX,j*timetravel.levelMap.textureSizeY)
-			end
+		if char == "a" then
+			return 0,0
+		elseif char == "b" then
+			return 0,1
+		elseif char == "c" then
+			return 0,2
+		else
+			return 1,0
 		end
 	end
 end
 
-function timetravel.levelMap:draw()
-	love.graphics.draw(mapBatch)
+function LevelMap:__init(mapSizeX, mapSizeY, mapString)
+	self.mapSizeX, self.mapSizeY = mapSizeX, mapSizeY
+	self.mapImage = love.graphics.newImage("gfx/tileset.png")
+	self.mapBatch = love.graphics.newSpriteBatch(self.mapImage,self.mapSizeX*self.mapSizeY)
+	self.quad = love.graphics.newQuad(0,0,timetravel.levelMap.textureSizeX,timetravel.levelMap.textureSizeY,
+										self.mapImage:getWidth(), self.mapImage:getHeight())
+	if (mapString == nil) or not(string.len(mapString)==mapSizeX*mapSizeY) then -- map string has the wrong size or does not exist
+		if not(mapString == nil) and not(string.len(mapString)==mapSizeX*mapSizeY) then
+			print("WARNING: map string has the wrong length ("..string.len(mapString).."), should be "..mapSizeX*mapSizeY)
+		end
+		-- TODO generate custom map string
+		local tempMapString = {}
+		for i=0,mapSizeX*mapSizeY do
+			tempMapString[i] = getCharFromNumber(love.math.random(0,4)) --TODO replace 4 with maximum number of tiles
+		end
+		mapString = table.concat(tempMapString)
+		--print(mapString)
+	end
+	self.defaultMapString = mapString
+	self.currentMapString = mapString
+	LevelMap:load(mapString, mapSizeX, mapSizeY, self.quad, self.mapBatch)
+end
+
+-- load map from given string
+function LevelMap:load(mapString, sizeX, sizeY, loadQuad, loadBatch)
+	loadBatch:clear()
+	for i=0,sizeX-1 do
+		for j=0,sizeY-1 do
+			--print("drawing map from string")
+			local textureX, textureY = getTileFromChar(mapString:sub(1+j+i*sizeX, 1+j+i*sizeX))
+			--print(textureX..", "..textureY)
+			loadQuad:setViewport(textureX*timetravel.levelMap.textureSizeX,textureY*timetravel.levelMap.textureSizeY,
+								timetravel.levelMap.textureSizeX,timetravel.levelMap.textureSizeY)
+			loadBatch:add(loadQuad,i*timetravel.levelMap.textureSizeX,j*timetravel.levelMap.textureSizeY)
+		end
+	end
+end
+
+function LevelMap:reload()
+	LevelMap:load(self.currentMapString, self.mapSizeX, self.mapSizeY, self.quad, self.mapBatch)
+end
+
+function LevelMap:draw()
+	love.graphics.draw(self.mapBatch)
+end
+
+function LevelMap:changeTile(posX, posY) -- position is given in pixels
+	--print("button pressed at "..posX..", "..posY)
+	local actualX, actualY = (posX-(posX%timetravel.levelMap.textureSizeX))/timetravel.levelMap.textureSizeX,
+							 (posY-(posY%timetravel.levelMap.textureSizeY))/timetravel.levelMap.textureSizeY
+	print("placing tile at "..actualX..", "..actualY)
+	self.currentMapString = util.replaceChar(self.currentMapString,getCharFromNumber(timetravel.levelMap.editor.currentTile),
+												1+actualY+actualX*self.mapSizeY)
+	self:reload()
+end
+
+function LevelMap:printMapString()
+	print(self.currentMapString)
+end
+
+function LevelMap:selectNextTile()
+	if timetravel.levelMap.editor.currentTile < timetravel.levelMap.editor.maxTileNumber then
+		timetravel.levelMap.editor.currentTile = timetravel.levelMap.editor.currentTile+1
+	else
+		timetravel.levelMap.editor.currentTile = 0
+	end
+	print("selected tile "..timetravel.levelMap.editor.currentTile)
+end
+
+function LevelMap:selectPrevTile()
+	if timetravel.levelMap.editor.currentTile > 0 then
+		timetravel.levelMap.editor.currentTile = timetravel.levelMap.editor.currentTile-1
+	else
+		timetravel.levelMap.editor.currentTile = timetravel.levelMap.editor.maxTileNumber
+	end
+	print("selected tile "..timetravel.levelMap.editor.currentTile)
 end
--- main.lua
+++ main.lua
@@ -1,20 +1,23 @@
 timetravel = {}
 
+require('lib/32log')
+require('lib/util')
 require('lib/levelMap')
 require("lib/object_manager")
 require("lib/quests")
 require("lib/player")
+timetravel.currentMode = 2 -- 2=in game, 8=edit mode
 
 function love.load()
 	IMAGE_PLAYER = love.graphics.newImage("gfx/player.png")
 	IMAGE_PART = love.graphics.newImage("gfx/part.png")
 	IMAGE_TIMEMACHINE = love.graphics.newImage("gfx/timemachine.png")
 
-	timetravel.levelMap:__init(4, 4, "blaclacalababbbb")
+	timetravel.currentMap = LevelMap:new(32, 32, nil)
 	player = CreatePlayer(IMAGE_PLAYER, 64, 128)
 	objectManager = CreateObjectManager()
 	objectManager:add(IMAGE_TIMEMACHINE, 128, 128, "GoToThePast")
-
+	
 	for i=0,15 do
 		objectManager:add(IMAGE_PART, math.random(0, 700), math.random(0, 500), "AddPart", true)
 	end
@@ -24,12 +27,17 @@ end
 
 function love.draw()
 	-- WORLD
-	timetravel.levelMap:draw()
+	timetravel.currentMap:draw()
 	objectManager:draw()
 	player:draw()
 	
 	-- HUD
 	love.graphics.print(GAME_PARTS, 256, 16)
+	if timetravel.currentMode == 8 then
+		love.graphics.print("EDIT MODE")
+	--else
+		--love.graphics.print("hello world")
+	end
 end
 
 function love.update(dt)
@@ -37,6 +45,19 @@ function love.update(dt)
 		player:moveLeft(dt)
 	elseif love.keyboard.isDown("d") then
 		player:moveRight(dt)
+	elseif love.keyboard.isDown("e") then --TODO only allow this in the main menu
+		-- switch between editor mode and game mode
+		if timetravel.levelMap.editor.lastKeyPress < os.time() then -- todo remove this once the update frequency is checked somewhere else
+			if timetravel.currentMode == 2 then
+				print("switching to edit mode")
+				timetravel.currentMode = 8
+			elseif timetravel.currentMode == 8 then
+				timetravel.currentMode = 2 
+			end
+			timetravel.levelMap.editor.lastKeyPress = os.time()
+		end
+	elseif love.keyboard.isDown("x") then -- export map string, only on console for now
+		timetravel.currentMap:printMapString()
 	end
 	
 	if love.keyboard.isDown("w") then
@@ -44,7 +65,39 @@ function love.update(dt)
 	elseif love.keyboard.isDown("s") then
 		player:moveDown(dt)
 	end
+	if timetravel.currentMode == 8 then
+		if timetravel.levelMap.editor.mousepressed == "l" then -- TODO quick fix from below, part 2
+			timetravel.currentMap:changeTile(love.mouse.getX(), love.mouse.getY())
+		end
+	end
 	
 	player:update(dt)
 	objectManager:checkCollision(player)
+end
+
+function love.mousepressed(x, y, button)
+	if timetravel.currentMode == 8 then
+		timetravel.levelMap.editor.mousepressed = button -- TODO quick fix for now
+		if button == "l" then
+			--timetravel.currentMap:changeTile(x, y)
+		elseif button == "r" then
+			-- change to object placement mode
+		elseif button == "wu" then
+			-- next tile
+			timetravel.currentMap:selectNextTile()
+		elseif button == "wd" then
+			-- previous tile
+			timetravel.currentMap:selectPrevTile()
+		elseif button == "m" then
+			-- grab tile
+		end
+	end
+end
+
+function love.mousereleased(x, y, button)
+	if timetravel.currentMode == 8 then
+		if button == "l" then
+			timetravel.levelMap.editor.mousepressed = ""
+		end
+	end
 end
